dd0b7759da9dabafc8a306f6eeb00999dc803e34
rajputaradhna <aradhnatnsingh01@gmail.com>
2021-11-29 00:52:55 +0530

    insertion operation

---
+++ LinkedList.java
@@ -0,0 +1,62 @@
+
+public class LinkedList{
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public LinkedList(){
+        this.size = 0;
+
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if(tail == null){
+            tail = head;
+        }
+        size += 1;
+    }
+    
+
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.print("END");
+    }
+    private class Node{
+       private int value;
+       private Node next;
+
+       public Node(int value){
+        this.value = value;
+        }
+
+        public Node(int value, Node next){
+        this.value = value;
+        this.next = next;
+
+        }
+    }
+
+
+public static void main(String[] args){
+    
+    LinkedList linkedList = new LinkedList();
+    linkedList.insertFirst(3);
+    linkedList.insertFirst(6);
+    linkedList.insertFirst(9);
+    linkedList.insertFirst(17);
+
+    linkedList.display();
+
+
+    
+}
+
+}
